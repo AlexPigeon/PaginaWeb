0f043ab7bd3c8d9b303ced0d37a9a46d05fa4f5e
T.Felipe <146483340+AlexPigeon@users.noreply.github.com>
2023-10-06 13:52:55 -0600

    subiendo carpeta docs

--- docs/main.js
+++ docs/main.js
@@ -1,19 +1,119 @@
-const arreloDeBotones = document.querySelectorAll("button")
-const numeroHtml = document.querySelector(".flex>h2")
-
-let numeroReal = 0
-numeroHtml.innerText = numeroReal
+import data from './data.js'
 
 
-function Sumar(sumando){
-    numeroReal += sumando
-    numeroHtml.innerText = numeroReal
+const divMain = document.getElementById("main")  
+console.log(data)
+
+
+
+data.map((data) => {
+    divMain.innerHTML += `<div class="flex">
+                            <div class ="pic">
+                            <img src=${data.img}>
+                            </div>
+                            <div class ="data">
+                             <h1>${data.title}</h1>
+                             <h2>${data.price}$<h2>
+                             <p>${data.desc}</p>
+                             <hr>
+                             </div>
+                            </div>`
+  
+})
+
+const platillos = document.getElementById("platillos")
+const postres = document.getElementById("postres")
+const bebidas = document.getElementById("bebidas")
+const todos = document.getElementById("todos")
+
+platillos.addEventListener('click', Platillos)
+postres.addEventListener('click', Postres)
+bebidas.addEventListener('click', Bebidas)
+todos.addEventListener('click', Todos)
+
+function Platillos(evento){
+
+    divMain.innerHTML = ""
+
+    let categoryfiltrada = 'platillo'
+    let filtrados = data.filter((data) => data.category === categoryfiltrada)
+    filtrados.forEach((data) => {
+        divMain.innerHTML += `<div class="flex">
+                                <div class ="pic">
+                                <img src=${data.img}>
+                                </div>
+                                <div class ="data">
+                                 <h1>${data.title}</h1>
+                                 <h2>${data.price}$<h2>
+                                 <p>${data.desc}</p>
+                                 </div>
+                                </div>`
+      
+    })
+    
 }
 
-arreloDeBotones.forEach((boton)=>{
-    boton.addEventListener('click',(evento)=>{
-        let numero = Number(evento.target.innerText)
-        Sumar(numero)
-        console.log(numero)
+function Postres(evento){
+
+    divMain.innerHTML = ""
+
+    let categoryfiltrada = 'postre'
+    let filtrados = data.filter((data) => data.category === categoryfiltrada)
+    filtrados.forEach((data) => {
+        divMain.innerHTML += `<div class="flex">
+                                <div class ="pic">
+                                <img src=${data.img}>
+                                </div>
+                                <div class ="data">
+                                 <h1>${data.title}</h1>
+                                 <h2>${data.price}$<h2>
+                                 <p>${data.desc}</p>
+                                 </div>
+                                </div>`
+      
     })
-})
+    
+}
+
+function Bebidas(evento){
+
+    divMain.innerHTML = ""
+
+    let categoryfiltrada = 'bebida'
+    let filtrados = data.filter((data) => data.category === categoryfiltrada)
+    filtrados.forEach((data) => {
+        divMain.innerHTML += `<div class="flex">
+                                <div class ="pic">
+                                <img src=${data.img}>
+                                </div>
+                                <div class ="data">
+                                 <h1>${data.title}</h1>
+                                 <h2>${data.price}$<h2>
+                                 <p>${data.desc}</p>
+                                 </div>
+                                </div>`
+      
+    })
+    
+}
+
+function Todos(evento){
+
+    divMain.innerHTML = ""
+    data.map((data) => {
+        divMain.innerHTML += `<div class="flex">
+                                <div class ="pic">
+                                <img src=${data.img}>
+                                </div>
+                                <div class ="data">
+                                 <h1>${data.title}</h1>
+                                 <h2>${data.price}$<h2>
+                                 <p>${data.desc}</p>
+                                 </div>
+                                </div>`
+      
+    })
+    
+    
+}
+
